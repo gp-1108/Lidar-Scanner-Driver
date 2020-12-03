f6591b29a7e4ae1e6a801eb7b0940f1cff0956be
gp-1108 <pietrogirotto00@gmail.com>
2020-12-03 10:24:43 +0000

    Backup

--- LaserScannerDriver.cpp
+++ LaserScannerDriver.cpp
@@ -1,11 +1,16 @@
 #include "LaserScannerDriver.h"
 
+
+/*
+ *  Main constructor with sensor resolution as argument. It's the main constructor used as any kind of other constructor of this classs relies
+ *  indirectly onto this one. The angle resolution must belong to [0,1], the internal buffer get initialized to an array of nullptr.
+ */
 LaserScannerDriver::LaserScannerDriver(const double sens) {
-	if (sens < 0 || sens > 1) {
-		throw std::out_of_range("Invalid angle resolution, angle must belong to ]0,1]");
+	if (sens < 0.1 || sens > 1) {
+		throw std::out_of_range("Invalid angle resolution, angle must belong to [0.1,1]");
 	}
 	resolution = sens;
-	/*
+    /*
 		The next two index will be initialized to an invalid index for two reasons:
 		-being able to tell if an object is being ispected by any means directly after initialization
 		-to set their values to appropriate number as in new_scan() they get increased before usage
@@ -14,16 +19,16 @@ LaserScannerDriver::LaserScannerDriver(const double sens) {
 	firstScanIndex = -1;
 	
 
-	/*
-		creating a pointer to an array of pointers to save data
-		no need to save angles because they can be retrieved as index*resolution
-	*/
 	scans = new double* [BUFFER_DIM];
 	for (int i = 0; i < BUFFER_DIM; i++) {
 		scans[i] = nullptr; //nullptr initalization to have a distinct invalid value without needing to allocate memory
 	}
 }
 
+/*
+	Copy constructor and assignment follow the deep copy philosophy as far as copying the buffer indexes to ensure an
+	identical behavior in the object copied compared to the original one.
+*/
 LaserScannerDriver::LaserScannerDriver(const LaserScannerDriver& drive) {
 	
 	/*
@@ -42,12 +47,12 @@ LaserScannerDriver::LaserScannerDriver(const LaserScannerDriver& drive) {
 
 	/*
 		As "scanNumber" is widely used in this class it will be commented only once (here):
-		scanNumber represents the number of different measures in a single scan, therefore it's 180/resolution. The int explicit conversion
-		is used to "Math.floor" the number obtained in case 180 is not multiple of resolution. It's consistent with the class usage as the 
-		last decimal in the division would mean we only got to measure just part of the distance. this is not only counter-intuitive but also
-		very imprecise, hence the decision to only save complete measurements.
+		scanNumber represents the number of different measures in a single scan including angle 180, therefore it's approx 180/resolution. 
+		The int explicit conversion is used to "Math.floor" the number obtained in case 180 is not multiple of 
+		resolution and the +1 to "Math.ceil" 180/resolution to allow for the measurement at angle 180. 
 	*/
-	int scanNumber = (int)(180 / resolution);
+	int scanNumber = (static_cast<int>(180 / resolution))+1;
+
 
 	for (int i = 0; i < BUFFER_DIM; i++) {
 		if (drive.scans[i] != nullptr) {
@@ -59,47 +64,71 @@ LaserScannerDriver::LaserScannerDriver(const LaserScannerDriver& drive) {
 	}
 }
 
+/*
+	Move Constructor: As it's a constructor there's no need to de-allocate scans or scans elements. The indexes get moved as well.
+  
+ */
 LaserScannerDriver::LaserScannerDriver(LaserScannerDriver&& drive) {
-	scans = new double* [BUFFER_DIM];
-	for (int i = 0; i < BUFFER_DIM; i++) {
-		scans[i] = nullptr;
-	}
+
 	resolution = drive.resolution;
 	bufferIndex = drive.bufferIndex;
 	firstScanIndex = drive.firstScanIndex;
-	int scanNumber = (int)(180 / resolution);
-	for (int i = 0; i < BUFFER_DIM; i++) {
-		if (drive.scans[i] != nullptr) {
-			scans[i] = new double[scanNumber];
-			for (int j = 0; j < scanNumber; j++) {
-				scans[i][j] = drive.scans[i][j];
-			}
-		}
-	}
-	drive.clear_buffer();
+	scans = drive.scans;
+
+	drive.scans = nullptr;
+	drive.bufferIndex = -1;
+	drive.firstScanIndex = -1;
 }
 
+
+/*
+ * Object Distructor: it ensures to clear all data allocated with new[] by constructor and/or assignment.
+ * 
+ */
 LaserScannerDriver::~LaserScannerDriver() {
-	for (int i = 0; i < BUFFER_DIM; i++) {
+
+	for (int i = 0; i < BUFFER_DIM && scans!=nullptr; i++) {
 		delete[] scans[i];
 	}
 	delete[] scans;
 }
 
+/*
+ * new_scan() inserts a vector<double> of measures into the main memory (scans) onto the index bufferIndex + 1 (updated on the firs line).
+ * If the data are not enoguh to fill the entire space allocated in the array (scanNuber) then they get initialized to zero, on the contrary 
+ * if there are more data then needed last measures don't get saved.
+ * 
+ */
 void LaserScannerDriver::new_scan(const std::vector<double>& vec) {
+	/*
+		bufferIndex is used as the last position where data had been inserted. This behavior makes the get_distance() method much
+		more intuitive. It requires the index to be incremented immediately before insertion.
+	*/
 	bufferIndex++;
+	
+	/*
+		Checking wether the buffer has ever been used, if not then sets firstScan Index to the correct position.
+	*/
 	if (firstScanIndex == -1) {
 		firstScanIndex = 0;
 	}
-	if (bufferIndex >= BUFFER_DIM) {
+
+	
+	if (bufferIndex >= BUFFER_DIM) {        //circular-buffer
 		bufferIndex = 0;
 	}
 
+	/*
+		Check if the most-recent (bufferIndex) and least-recent (firstScanIndex) indexes are the same -> buffer full or empty
+	*/
 	if (bufferIndex == firstScanIndex) {
-		if (scans[firstScanIndex] == nullptr) {
-
-		}
-		else {
+		/*
+			There are two possibilities:
+			1. Buffer is full (scans[firstScanIndex] it's not empty (=nullptr)) then we need to move firstScanIndex as we are updating those data
+			soon with another scan
+			2. Buffer is empty then it's correct to have most-recent and least-recent points to the new scan insertion. No need to change anything
+		*/
+		if(scans[firstScanIndex] != nullptr) {	
 			firstScanIndex++;
 			if (firstScanIndex >= BUFFER_DIM) {
 				firstScanIndex = 0;
@@ -108,10 +137,10 @@ void LaserScannerDriver::new_scan(const std::vector<double>& vec) {
 	}
 
 
-	int vecSize = vec.size();
-	int scanNumber = (int)(180 / resolution); //using int explicit conversion to round-down the number of measures per scan to an integer
+	int vecSize = vec.size();   //called once as it will be later used multiple times.
+	int scanNumber = (static_cast<int>(180 / resolution))+1; //using int explicit conversion to round-down the number of measures per scan to an integer
 	
-	//checking whether the array has already been initialized or not
+	//checking whether the array has already been created or not
 	if (scans[bufferIndex] == nullptr) {
 		scans[bufferIndex] = new double[scanNumber];
 	}
@@ -121,20 +150,37 @@ void LaserScannerDriver::new_scan(const std::vector<double>& vec) {
 			scans[bufferIndex][i] = vec[i];
 			i++;
 		}
-		if (i >= scanNumber) {
+		if (i >= scanNumber) {  //to much data
 			break;
 		}
-		scans[bufferIndex][i] = 0;
+		scans[bufferIndex][i] = 0; //not enough data
 	}
 }
 
-std::ostream& operator<<(std::ostream& os, LaserScannerDriver& driver) {
-	int scanNumber = (int)(180 / driver.resolution);
-	for (int i = 0; i < scanNumber; i++) {
-		os << driver.scans[driver.bufferIndex][i] << " ";
+
+/* 
+ *  operator<< has been implemented with the aid of two different public function members to avoid friend.
+ *  Rather than using ger_resolution() and get_distance() and saving some performance (not on an asymptotic scale) here is used a 
+ *  get_recent_scan() which returns a vector<double> of the most recent scan. This choice has been made to improve class 
+ * usage by implementing potentially useful function member without any asymptotic penalty.
+ * 
+ */
+std::ostream& operator<<(std::ostream& os,const LaserScannerDriver& driver) {
+    
+	if (driver.is_empty()) {
+		os << "Empty Buffer";
+		return os;
+	}
+	double angle = 0;
+	double res = driver.get_resolution();
+	for (int i = 0; angle <= 180; i++) {
+		angle = i * res;
+		os << driver.get_distance(angle) << " ";
 	}
 	return os;
 }
+
+
 
 void LaserScannerDriver::clear_buffer() {
 	for (int i = 0; i < BUFFER_DIM; i++) {
@@ -148,11 +194,11 @@ void LaserScannerDriver::clear_buffer() {
 
 std::vector<double> LaserScannerDriver::get_scan() {
 
-	if (scans[bufferIndex] == nullptr) {
+	if (bufferIndex == -1 || scans[bufferIndex] == nullptr) {
 		throw std::runtime_error("Trying to recover data from empty driver");
 	}
 
-	int scanNumber = (int)(180 / resolution);
+	int scanNumber = (static_cast<int>(180 / resolution))+1;
 
 	std::vector<double> firstScan;
 
@@ -180,9 +226,11 @@ double LaserScannerDriver::get_distance(const double angle) const {
 	}
 
 	double rawIndex = angle / resolution;
-	int flooredIndex = (int)rawIndex;
+	int flooredIndex = static_cast<int>(rawIndex);
 
-	if (flooredIndex - rawIndex >= 0.5) {
+    int scanNumber = (static_cast<int>(180/resolution))+1;
+    
+	if (rawIndex - flooredIndex >= 0.5 && rawIndex<=(scanNumber-1)) {
 		return scans[bufferIndex][flooredIndex + 1];
 	}
 
@@ -192,7 +240,7 @@ double LaserScannerDriver::get_distance(const double angle) const {
 LaserScannerDriver& LaserScannerDriver::operator=(const LaserScannerDriver& drive) {
 	double** newScans = new double* [BUFFER_DIM];
 	resolution = drive.resolution;
-	int scanNumber = (int)(180 / resolution);
+	int scanNumber = (static_cast<int>(180 / resolution))+1;
 	for (int i = 0; i < BUFFER_DIM; i++) {
 		if (drive.scans[i] == nullptr) {
 			newScans[i] = nullptr;
@@ -205,7 +253,15 @@ LaserScannerDriver& LaserScannerDriver::operator=(const LaserScannerDriver& driv
 		}
 	}
 
-	clear_buffer();
+	/*
+		Freeing memory before giving new values to prevent memory leaks
+	*/
+	for (int i = 0; i < BUFFER_DIM; i++) {
+		delete[] scans[i];
+	}
+	delete[] scans;
+
+
 	bufferIndex = drive.bufferIndex;
 	firstScanIndex = drive.firstScanIndex;
 
@@ -215,29 +271,28 @@ LaserScannerDriver& LaserScannerDriver::operator=(const LaserScannerDriver& driv
 }
 
 LaserScannerDriver& LaserScannerDriver::operator=(LaserScannerDriver&& drive) {
-	double** newScans = new double* [BUFFER_DIM];
-	resolution = drive.resolution;
-	int scanNumber = (int)(180 / resolution);
-	for (int i = 0; i < BUFFER_DIM; i++) {
-		if (drive.scans[i] == nullptr) {
-			newScans[i] = nullptr;
-		}
-		else {
-			newScans[i] = new double[scanNumber];
-			for (int j = 0; j < scanNumber; j++) {
-				newScans[i][j] = drive.scans[i][j];
-			}
-		}
-	}
 
-	clear_buffer();
+	for (int i = 0; i < BUFFER_DIM; i++) {
+		delete[] scans[i];
+	}
+	delete[] scans;
+
+	scans = drive.scans;
+	resolution = drive.resolution;
 	bufferIndex = drive.bufferIndex;
 	firstScanIndex = drive.firstScanIndex;
 
-	scans = newScans;
-
-	drive.clear_buffer();
+	drive.bufferIndex = -1;
+	drive.firstScanIndex = -1;
+	drive.scans = nullptr;
 
 	return *this;
 }
 
+double LaserScannerDriver::get_resolution() const{
+	return resolution;
+}
+
+bool LaserScannerDriver::is_empty() const {
+	return false;
+}
--- LaserScannerDriver.h
+++ LaserScannerDriver.h
@@ -9,17 +9,17 @@ private:
 	double resolution;
 public:
 	/*
-		LaserScannerDriver class doesn't have a default constructor. That's due to a Developer choice: it doesn't make sense to 
+		LaserScannerDriver class doesn't have a default constructor. That's due to a Developer choice: it doesn't make sense to
 		intialize a drive for a sensor without the main infos about the sensor itself. Also it's an error-prone behavior because
 		it could lead to data insertions witout actually knowing how many data will be saved and to which angle they correspond.
 		Therefore it is thought to have precise data when initalizing a Driver.
 	*/
 
 	/*
-		BUFFER_DIM constant is declared in the public section for logic reasons. Any class-utilizer can see the buffer dimension and (if needed) 
+		BUFFER_DIM constant is declared in the public section for logic reasons. Any class-utilizer can see the buffer dimension and (if needed)
 		write re-usable code without the hassle of changing code whenever he/she modifies the buffer size in the class header.
 	*/
-	const int BUFFER_DIM = 3; 
+	const int BUFFER_DIM = 10;
 
 	LaserScannerDriver& operator=(const LaserScannerDriver& drive);
 
@@ -30,18 +30,21 @@ public:
 	LaserScannerDriver(const double sens);
 
 	LaserScannerDriver(const LaserScannerDriver& drive);
-	
+
 	~LaserScannerDriver();
-	
+
 	void new_scan(const std::vector<double>& vec);
 
 	void clear_buffer();
 
-	std::vector<double> get_scan(); 
+	std::vector<double> get_scan();
 
 	double get_distance(const double angle) const;
-	
-	friend std::ostream& operator<<(std::ostream& os, LaserScannerDriver& const driver);
-};
 
+	double get_resolution() const;
+
+	bool is_empty() const;
+};
+	
+std::ostream& operator<<(std::ostream& os, const LaserScannerDriver& driver);
 
